8d2d50379b043fadc951f2ff22e91987fd713770
nmelnychenko <mykola.melnichenko@gmail.com>
2017-07-24 15:43:22 +0300

    make worked slider

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-var mask = document.querySelectorAll('.underlay');
+var wrapper = document.getElementById("js-carousel__wrap");
+var next = document.getElementById("next");
+var previous = document.getElementById("previous");
+var pageNumber = document.getElementById("counter");
+var pageCounter = 1;
 
-console.log(mask);
+function changeSlide (element, number, counter) {
+    element.style.marginLeft = (-100)*(counter-1) + 'vw';
+    number.innerHTML = counter.toString();
+}
+
+previous.onclick = function () {
+    pageCounter > 1 ? --pageCounter : pageCounter = 3;
+    changeSlide(wrapper, pageNumber, pageCounter);
+};
+
+next.onclick = function () {
+    pageCounter < 3 ? ++pageCounter : pageCounter = 1;
+    changeSlide(wrapper, pageNumber, pageCounter);
+};
